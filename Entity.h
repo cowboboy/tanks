e41f8648a4b8f3c81062531ceeab26517245e114
goodsmile2004 <hilnar960@gmail.com>
2022-03-10 19:18:18 +0500

    m

--- Entity.h
+++ Entity.h
@@ -8,6 +8,8 @@ public:
 	sf::Texture m_texture;
 	sf::Sprite m_sprite;
 	sf::Sprite m_spriteTurret;
-	Entity(std::string f, std::string n);
+	sf::Vector2f m_coords;
+	float m_speedMovement, m_speedRotate, m_speedRotateTurret, m_maxSpeedMovement, m_acceleration, m_angle, m_angleTurret, m_angleTargetTurret;
+	Entity(std::string f, std::string n, sf::Vector2f c, float maxSM, float a, float sR, float sRT);
 };
 
